5f0c30c9ae5be043c231ef07a53e21c3af169740
Tamelien <Tamelien@protonmail.com>
2025-11-06 22:43:06 +0100

    feat(main.py): add game clock and frame timing

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    dt = 0
+    clock = pygame.time.Clock()
+
     while True:
         log_state()
         
@@ -16,6 +20,8 @@ def main():
             
         screen.fill("black", rect=None, special_flags=0)
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
