016c60975777c0a12c719d1fffc97e6b2223ea24
Tamelien <Tamelien@protonmail.com>
2025-11-06 17:50:47 +0100

    feat(main.py): integrate logger and main game loop

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import pygame
-from constants import *
+from constants import (SCREEN_HEIGHT, 
+                       SCREEN_WIDTH,)
+from logger import log_state
+
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill("black", rect=None, special_flags=0)
+        pygame.display.flip()
+    
 if __name__ == "__main__":
     main()
