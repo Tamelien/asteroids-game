3fd98e3f4e91889737ca9ec5a57d0c20c10b6b96
Tamelien <Tamelien@protonmail.com>
2025-11-07 02:22:45 +0100

    feat(asteroidfield.py): add AsteroidField class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from logger import log_state
 from player import Player
 from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import (SCREEN_HEIGHT, 
                        SCREEN_WIDTH,)
 
@@ -13,11 +14,13 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    astroids = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
-    Asteroid.containers = (astroids, updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    astroidfield = AsteroidField()
 
     dt = 0
     clock = pygame.time.Clock() 
