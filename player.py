9caebb203caa160fedfa853d518bf1f12783d048
Tamelien <Tamelien@protonmail.com>
2025-11-07 19:20:09 +0100

    feat(player.py): implement shooting mechanic

--- player.py
+++ player.py
@@ -1,13 +1,17 @@
 import pygame
+from shot import Shot
 from circleshape import CircleShape
 from constants import (PLAYER_RADIUS, 
                        PLAYER_TURN_SPEED,
-                       PLAYER_SPEED)
+                       PLAYER_SPEED,
+                       PLAYER_SHOOT_SPEED,
+                       PLAYER_SHOOT_COOLDOWN)
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,8 +31,13 @@ class Player(CircleShape):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED 
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shot_cooldown_timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(dt * (-1))
@@ -38,4 +47,9 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt * (-1))
+        if keys[pygame.K_SPACE]:
+            if self.shot_cooldown_timer < 0:
+                self.shoot()
+                self.shot_cooldown_timer = PLAYER_SHOOT_COOLDOWN
+        
         
